93da48fb04d7569d579a74fcc48d8ecbaedc8ec7
Peter Marosi <marosip1@hotmail.com>
2022-05-09 15:22:03 +0200

    login and logout implemented in store

---
+++ src/stores/admin-auth.ts
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const adminAuthStore = defineStore('admin-auth', {
+  state: () => {
+    return {
+      accessToken: '',
+      loggedIn: false
+    }
+  },
+
+  getters: {
+    authenticated: (state) => state.loggedIn,
+  },
+
+  actions: {
+    async login () {
+      const URL = 'https://okoshaccp-integ-2jdqz.ondigitalocean.app/app/auth/login';
+
+      axios.post(URL, {
+        username: 'elek',
+        passcode: '034125'
+      }).then( res => {
+        this.$patch({
+          accessToken: res.data.accessToken,
+          loggedIn: true
+        })
+      }).catch( err => {
+        console.log(err);
+        this.$patch({
+          accessToken: 'res.data.accessToken',
+          loggedIn: true
+        })
+      });
+    },
+    logout () {
+      this.$patch({
+        accessToken: '',
+        loggedIn: false
+      })
+    }
+  }
+});
